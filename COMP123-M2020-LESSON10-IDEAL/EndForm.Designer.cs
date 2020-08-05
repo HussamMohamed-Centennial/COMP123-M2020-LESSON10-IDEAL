e121563cbf4450761fe8f5351b7a4e84cc2bd01c
Hussam Eldin Mohamed <dr.hussam@live.com>
2020-08-05 00:25:35 -0400

    added End Form

--- COMP123-M2020-LESSON10-IDEAL/EndForm.Designer.cs
+++ COMP123-M2020-LESSON10-IDEAL/EndForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace COMP123_M2020_LESSON10_IDEAL
 {
-    partial class MainForm
+    partial class EndForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,61 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.MainFormLabel = new System.Windows.Forms.Label();
-            this.NextButton = new System.Windows.Forms.Button();
+            this.EndFormLabel = new System.Windows.Forms.Label();
             this.BackButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // MainFormLabel
+            // EndFormLabel
             // 
-            this.MainFormLabel.Location = new System.Drawing.Point(12, 183);
-            this.MainFormLabel.Name = "MainFormLabel";
-            this.MainFormLabel.Size = new System.Drawing.Size(600, 45);
-            this.MainFormLabel.TabIndex = 3;
-            this.MainFormLabel.Text = "Main Form";
-            this.MainFormLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // NextButton
-            // 
-            this.NextButton.Location = new System.Drawing.Point(462, 379);
-            this.NextButton.Name = "NextButton";
-            this.NextButton.Size = new System.Drawing.Size(150, 50);
-            this.NextButton.TabIndex = 2;
-            this.NextButton.Text = "Next";
-            this.NextButton.UseVisualStyleBackColor = true;
+            this.EndFormLabel.Location = new System.Drawing.Point(6, 183);
+            this.EndFormLabel.Name = "EndFormLabel";
+            this.EndFormLabel.Size = new System.Drawing.Size(600, 45);
+            this.EndFormLabel.TabIndex = 5;
+            this.EndFormLabel.Text = "End Form";
+            this.EndFormLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // BackButton
             // 
-            this.BackButton.Location = new System.Drawing.Point(18, 379);
+            this.BackButton.Location = new System.Drawing.Point(12, 379);
             this.BackButton.Name = "BackButton";
             this.BackButton.Size = new System.Drawing.Size(150, 50);
-            this.BackButton.TabIndex = 2;
+            this.BackButton.TabIndex = 4;
             this.BackButton.Text = "Back";
             this.BackButton.UseVisualStyleBackColor = true;
             // 
-            // MainForm
+            // EndForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 441);
-            this.Controls.Add(this.MainFormLabel);
+            this.Controls.Add(this.EndFormLabel);
             this.Controls.Add(this.BackButton);
-            this.Controls.Add(this.NextButton);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.MaximizeBox = false;
-            this.Name = "MainForm";
+            this.Name = "EndForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "MainForm";
+            this.Text = "End Form";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label MainFormLabel;
-        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Label EndFormLabel;
         private System.Windows.Forms.Button BackButton;
     }
 }
