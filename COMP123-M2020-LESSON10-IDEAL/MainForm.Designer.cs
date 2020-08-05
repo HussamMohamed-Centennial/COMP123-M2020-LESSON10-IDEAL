e2f505ad4e5c5e7f16737cd14a77ecf24c2696b7
Hussam Eldin Mohamed <dr.hussam@live.com>
2020-08-04 23:58:11 -0400

    added main form

---
+++ COMP123-M2020-LESSON10-IDEAL/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace COMP123_M2020_LESSON10_IDEAL
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainFormLabel = new System.Windows.Forms.Label();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MainFormLabel
+            // 
+            this.MainFormLabel.Location = new System.Drawing.Point(12, 183);
+            this.MainFormLabel.Name = "MainFormLabel";
+            this.MainFormLabel.Size = new System.Drawing.Size(600, 45);
+            this.MainFormLabel.TabIndex = 3;
+            this.MainFormLabel.Text = "Main Form";
+            this.MainFormLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(462, 379);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(150, 50);
+            this.NextButton.TabIndex = 2;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(18, 379);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(150, 50);
+            this.BackButton.TabIndex = 2;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.MainFormLabel);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.NextButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MainFormLabel;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Button BackButton;
+    }
+}
